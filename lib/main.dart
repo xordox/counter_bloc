190b40c401a05042f4cd9e9f2785b078226f7972
Prabin Maharjan <coolprabin2000@gmail.com>
2023-01-05 12:14:51 +0545

    added hydratedBloc feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'bloc/counter/counter_barrel.dart';
 
-void main() {
-  Bloc.observer = CounterBlocObserver();
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = await HydratedStorage.build(storageDirectory: await getApplicationDocumentsDirectory());
+  
+  HydratedBlocOverrides.runZoned(
+    () => runApp(const MyApp()),
+    blocObserver: CounterBlocObserver(),
+    storage: storage,
+  );
 }
 
 class MyApp extends StatelessWidget {
